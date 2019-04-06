299ea5160f10f91c05ad16d7bd26b0f401d74945
thunderamur <thunder-amur@yandex.ru>
2019-04-06 18:19:35 +0900

    DRF Initial

---
+++ medical_ERP/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+from main.views import UserViewSet, GroupViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]

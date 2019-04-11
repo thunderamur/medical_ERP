07013ec55f75532cec2773f74d3dba5f9b12a072
thunderamur <thunder-amur@yandex.ru>
2019-04-11 17:53:07 +0900

    Throttle options added

--- medical_ERP/settings.py
+++ medical_ERP/settings.py
@@ -133,4 +133,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '3/minute',
+        'user': '60/minute',
+    }
 }

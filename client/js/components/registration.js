4fa95b4345c68bcab995e1f30fa913a6120853b2
thunderamur <thunder-amur@yandex.ru>
2019-04-16 18:25:41 +0900

    Первый вариант страницы логина готов

---
+++ client/js/components/registration.js
@@ -0,0 +1,22 @@
+Vue.component('registration', {
+	// data() {
+	// 	return {
+	// 		username: '',
+	// 		password: '',
+	// 	}
+	// },
+	// methods: {
+	// 	submit(){
+	// 		postJson(
+	// 			API,
+	// 			{"username":this.username,"password":this.password},
+	// 		)
+	// 		.then(data => {
+	// 			this.$emit('receive-token', data.token);
+	// 		});
+	// 	}
+	// },
+	template: `
+			<h1>Hello, World!</h1>
+		`,
+});

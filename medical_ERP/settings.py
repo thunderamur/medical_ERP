1f1c72c8b8e5c746d3fdc29e009bad2ac670f197
thunderamur <thunder-amur@yandex.ru>
2019-04-20 18:36:46 +0900

    It fucking server works now!

--- medical_ERP/settings.py
+++ medical_ERP/settings.py
@@ -136,6 +136,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',

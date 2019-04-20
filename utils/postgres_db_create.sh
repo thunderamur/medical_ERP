19f7768f334f968c83957519a7b2fb243dc99e09
thunderamur <thunder-amur@yandex.ru>
2019-04-20 16:38:25 +0900

    TimeZone fixed

--- utils/postgres_db_create.sh
+++ utils/postgres_db_create.sh
@@ -5,7 +5,7 @@ sudo -u postgres psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
     CREATE USER medical_erp_user WITH PASSWORD 'wNO2-L18q-MiU7';
     ALTER ROLE medical_erp_user SET client_encoding TO 'utf8';
     ALTER ROLE medical_erp_user SET default_transaction_isolation TO 'read committed';
-    ALTER ROLE medical_erp_user SET timezone TO 'UTC';
+    ALTER ROLE medical_erp_user SET timezone TO 'Asia/Yakutsk';
     GRANT ALL PRIVILEGES ON DATABASE medical_erp_db TO medical_erp_user;
     ALTER USER medical_erp_user CREATEDB;
 EOSQL

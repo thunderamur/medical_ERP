fdb343214dd2e6252d69f3391f75f7b12cfd2bf4
thunderamur <thunder-amur@yandex.ru>
2019-04-30 11:43:00 +0900

    New serializers. Base model changed.

--- api/serializers.py
+++ api/serializers.py
@@ -1,24 +1,56 @@
 from django.contrib.auth.models import User, Group
 
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import ModelSerializer
 
-from main.models import Record
+from main.models import Record, Passport, Post, Staff, Patient, Service
 
 
-class UserSerializer(HyperlinkedModelSerializer):
+class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = '__all__'
 
 
-class GroupSerializer(HyperlinkedModelSerializer):
+class GroupSerializer(ModelSerializer):
     class Meta:
         model = Group
-        fields = ('url', 'name')
+        fields = '__all__'
 
 
-class RecordSerializer(HyperlinkedModelSerializer):
+class RecordSerializer(ModelSerializer):
     class Meta:
         model = Record
-        fields = ('url', 'datetime', 'doctor', 'patient', 'created', 'updated')
+        fields = '__all__'
+        depth = 3
 
+
+class PassportSerializer(ModelSerializer):
+    class Meta:
+        model = Passport
+        fields = '__all__'
+
+
+class PostSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class StaffSerializer(ModelSerializer):
+    class Meta:
+        model = Staff
+        fields = '__all__'
+        depth = 2
+
+
+class PatientSerializer(ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = '__all__'
+        depth = 2
+
+
+class ServiceSerializer(ModelSerializer):
+    class Meta:
+        model = Service
+        fields = '__all__'
